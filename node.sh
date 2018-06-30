5707e544c7faa50fc349120fd1898b8ca8cc0ad9
Adan Toscano <adantoscano@gmail.com>
2018-07-01 01:59:54 +0200

    add node.sh

---
+++ node.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+sudo apt install -y curl
+
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.11/install.sh | bash
+
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+
+nvm install node --lts
