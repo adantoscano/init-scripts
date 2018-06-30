28ea28197fe6e5765aa7cfb946b587470c0f77ef
Adan Toscano <adantoscano@gmail.com>
2018-06-30 18:21:15 +0200

    add ssh-github

---
+++ ssh-github.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+echo "email: "
+
+read email
+
+mkdir ~/.ssh/keys
+
+ssh-keygen -t rsa -b 4096 -C "$email" -f ~/.ssh/keys/github-rsa
+
+eval "$(ssh-agent -s)"
+
+ssh-add ~/.ssh/keys/github-rsa
+
+echo "
+# GitHub.com server
+Host github.com
+IdentityFile ~/.ssh/keys/github-rsa
+" >> ~/.ssh/config
+
+echo "
+
+////////////////////////////////////////////////////////////////////
+1. go to https://github.com/settings/keys
+
+2. copy and paste the text below
+////////////////////////////////////////////////////////////////////
+
+"
+cat ~/.ssh/keys/github-rsa.pub
